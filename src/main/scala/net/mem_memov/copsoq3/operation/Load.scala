0535c8a42f10b2aaaa4fcf69e9b3cfaa7fe650b8
Alexander Novikov <anovikov@tsum.ru>
2023-07-27 17:07:41 +0300

    wip

--- src/main/scala/net/mem_memov/copsoq3/operation/Load.scala
+++ src/main/scala/net/mem_memov/copsoq3/operation/Load.scala
@@ -1,6 +1,6 @@
 package net.mem_memov.copsoq3.operation
 
-import net.mem_memov.copsoq3.{Command, DataSource, Operation, Questionnaire, QuestionEnumeration, Survey}
+import net.mem_memov.copsoq3.{Command, DataSource, Operation, Question, Questionnaire, QuestionEnumeration, Survey}
 
 case class Load(
   command: Command.Load.type
@@ -11,11 +11,25 @@ case class Load(
     println("Загрузка файла")
 
     val path = io.StdIn.readLine("Укажите путь к файлу в формате CSV: ")
-
     val dataSource = DataSource.csvFile(path)
+    val questionColumnMapOption = Load.readQuestionColumnMapOption(dataSource)
+
+    questionColumnMapOption match
+      case None =>
+        survey
+      case Some(questionColumnMap) =>
+        val questionnaires = Load.readQuestionnaires(dataSource, questionColumnMap)
+
+        questionnaires.foldLeft(survey){ (survey, questionnaire) =>
+          survey.addQuestionnaire(questionnaire)
+        }
+
+object Load:
+
+  def readQuestionColumnMapOption(dataSource: DataSource): Option[Map[Question, Int]] =
 
     val codeQuestionMap = QuestionEnumeration.toCodeQuestionMap
-    val questionColumnMapOption = dataSource.readFirst { row =>
+    dataSource.readFirst { row =>
       val codeQuestionOptions = row.map { cell =>
         codeQuestionMap.get(cell) match
           case Some(codeQuestion) =>
@@ -30,28 +44,23 @@ case class Load(
       codeQuestionOptions.zipWithIndex.filter(_._1.nonEmpty).map(pair => (pair._1.get, pair._2)).toMap
     }
 
-    questionColumnMapOption match
-      case None =>
-        survey
-      case Some(questionColumnMap) =>
-        val questionnaires = dataSource.read { (rowIndex, row) =>
-          QuestionEnumeration.getAll.toVector.foldLeft(Questionnaire.empty) { (questionnaire, questionEnumeration) =>
-            val question = questionEnumeration.getQuestion
-            val columnNumberOption = questionColumnMap.get(question)
-            columnNumberOption match
-              case None =>
-                questionnaire.addValueOption(question.code, None)
-              case Some(columnNumber) =>
-                if columnNumber >= row.length then
-                  questionnaire.addValueOption(question.code, None)
-                else
-                  val input = row(columnNumber - 1)
-                  val valueOption = question.scale.evaluate(input)
-                  questionnaire.addValueOption(question.code, valueOption)
-          }
-        }
+  def readQuestionnaires(dataSource: DataSource, questionColumnMap: Map[Question, Int]): Vector[Questionnaire] =
 
-        questionnaires.foldLeft(survey){ (survey, questionnaire) =>
-          survey.addQuestionnaire(questionnaire)
-        }
+    dataSource.read(readQuestionnaire(questionColumnMap))
 
+  def readQuestionnaire(questionColumnMap: Map[Question, Int])(rowIndex: Int, row: Vector[String]): Questionnaire =
+
+    QuestionEnumeration.getAll.toVector.foldLeft(Questionnaire.empty) { (questionnaire, questionEnumeration) =>
+      val question = questionEnumeration.getQuestion
+      val columnNumberOption = questionColumnMap.get(question)
+      columnNumberOption match
+        case None =>
+          questionnaire.addValueOption(question.code, None)
+        case Some(columnNumber) =>
+          if columnNumber >= row.length then
+            questionnaire.addValueOption(question.code, None)
+          else
+            val input = row(columnNumber - 1)
+            val valueOption = question.scale.evaluate(input)
+            questionnaire.addValueOption(question.code, valueOption)
+    }
